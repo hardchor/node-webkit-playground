014c39193723330000d28590f1f3007c8eaad71b
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2014-03-14 08:25:29 +0000

    working dummy page build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,25 @@
 module.exports = function (grunt) {
+
     // Project configuration.
     grunt.initConfig({
         pkg:        grunt.file.readJSON('package.json'),
         nodewebkit: {
-            version:   "0.9.2",
-            build_dir: "./build",
-            win:       false,
-            mac:       false,
-            linux32:   false,
-            linux64:   true
+            options: {
+                build_dir: './build',
+                mac:       false,
+                win:       false,
+                linux32:   false,
+                linux64:   true,
+//                credits: './credits.html', // mac only, used to display credits
+//                mac_icns: './app.icns'
+            },
+            src:     ['./app.nw/**/*'] // Your node-wekit app source directory
         }
     });
 
     grunt.loadNpmTasks('grunt-node-webkit-builder');
 
-    grunt.registerTask('build', ['grunt-node-webkit-builder']);
+    // Default task(s).
+    grunt.registerTask('default', ['nodewebkit']);
+
 };
