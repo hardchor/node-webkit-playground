11e9b4251b120a0b03b11cdcf6fd4a449512b472
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2014-03-14 08:59:55 +0000

    embedded skypoker as POC

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,12 @@ module.exports = function (grunt) {
         pkg:        grunt.file.readJSON('package.json'),
         nodewebkit: {
             options: {
+                version:   '0.9.2',
                 build_dir: './build',
                 mac:       false,
                 win:       false,
                 linux32:   false,
-                linux64:   true,
+                linux64:   true
 //                credits: './credits.html', // mac only, used to display credits
 //                mac_icns: './app.icns'
             },
